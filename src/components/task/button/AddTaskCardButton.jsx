c238998013a9df0e53ffd13ad0a07955921cce2c
kaity-kaity <kaito.kitagawa.biz@gmail.com>
2022-06-10 03:06:37 +0900

    done

---
+++ src/components/task/button/AddTaskCardButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { v4 as uuid } from 'uuid';
+
+export const AddTaskCardButton = ({ taskCardsList, setTaskCardsList }) => {
+  const taskCardId = uuid()
+
+  const addTaskCard = () => {
+    setTaskCardsList([...taskCardsList, { id: taskCardId, draggableId: `task${taskCardId}`}])
+  }
+
+  return (
+    <div className="addTaskCardButtonArea">
+      <button className="addTaskCardButton" onClick={addTaskCard}>+</button>
+    </div>
+  )
+}
---
+++ src/components/task/input/TaskAddInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { v4 as uuid } from 'uuid';
+
+export const TaskAddInput = ({inputText, setInputText, taskList, setTaskList}) => {
+
+
+  const handleSubmit = (e) => {
+    const taskId = uuid()
+    e.preventDefault()
+    if (inputText === '') return
+    setTaskList([...taskList, {id: taskId, draggableId: `task-${taskId}`, text: inputText,}])
+    setInputText('')
+  }
+
+  const handleChange = (e) => [
+    setInputText(e.target.value)
+  ]
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={inputText} className="taskAddInput" type="text" placeholder="add a task" onChange={handleChange}></input>
+      </form>
+    </div>
+  )
+}
